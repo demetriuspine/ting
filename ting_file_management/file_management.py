dae52e55d1158bdcbb433842de15d7060e4cf0c3
lucas <mr.pine97@gmail.com>
2022-08-08 11:22:08 -0300

    req02 - feat: implementa a função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
     """Aqui irá sua implementação"""
+    try:
+        if '.txt' not in path_file:
+            print("Formato inválido", file=sys.stderr)
+
+        with open(path_file, mode="r") as file:
+            content = file.read()
+            formatted_content = content.split("\n")
+            return formatted_content
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
