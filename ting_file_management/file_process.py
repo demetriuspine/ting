7a12587f71d6a02ad5b4367e9117b557321384f9
lucas <mr.pine97@gmail.com>
2022-08-08 11:37:01 -0300

    req03 - feat: implementa a função process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    instance_len = instance.__len__()
+    paths_list = []
+    if instance_len > 0:
+        for index in range(instance_len):
+            paths_list.append(instance.search(index)["nome_do_arquivo"])
+
+    if path_file not in paths_list:
+        content = txt_importer(path_file)
+        result = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(content),
+            "linhas_do_arquivo": content
+        }
+        instance.enqueue(result)
+        print(result, file=sys.stdout)
 
 
 def remove(instance):
